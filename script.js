22f08124377a2b77b632e1b2c80d24172303451f
Aleksandra <aleksandrapozarickaa@gmail.com>
2026-09-11 11:42:36 +0000

    Create function that allows users to fill the cells of the field

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const Gameboard = (() => {
+const gameboard = (() => {
     const board = ['', '', '', '', '', '', '', '', ''];
 
     const getBoard = () => board;
@@ -15,6 +15,37 @@ const Gameboard = (() => {
     return {getBoard, setCell};
 })()
 
-const Player = {
-
+const createPlayer = (name, marker) => {
+    return {name, marker}
 }
+
+const player1 = createPlayer('Player1', 'X')
+const player2 = createPlayer('Player2', 'O')
+
+const gameController = (() => {
+    const players = [player1, player2]
+    let activePlayerIndex = 0;
+
+    const getActivePlayer = () => players[activePlayerIndex]
+
+    const switchTurn = () => {
+        activePlayerIndex = activePlayerIndex === 0? 1: 0;
+    }
+
+    const playRound = (cellIndex) => {
+        const currentPlayer = getActivePlayer();
+        console.log(`${currentPlayer.name} moves to the cell ${cellIndex}`)
+
+        const wasSuccessful = gameboard.setCell(cellIndex, currentPlayer.marker)
+
+        if(wasSuccessful){
+            console.log(`Success! current board: ${gameboard.getBoard()}`)
+            switchTurn();
+        }else{
+            console.log('this cell is already occupied! Try another')
+        }
+    }
+
+    return {playRound}
+})();
+
